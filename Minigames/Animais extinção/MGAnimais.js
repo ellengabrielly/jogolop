5e690c32c11cc8efa659f2eb1c71f9a0693dce97
lucasboot <39107063+lucasboot@users.noreply.github.com>
2018-11-08 17:37:22 -0300

    Add files via upload

--- Minigames/Animais extinção/MGAnimais.js
+++ Minigames/Animais extinção/MGAnimais.js
@@ -3,7 +3,7 @@
 */
 
 var x, y, n;
-var tirox = [], tiroy = [], tirosBS = [], tirosAV = 4;
+var tirox = [], tiroy = [], tirosBS = [], tirosAV = 2, municao = 2;
 var ye = [];
 var xe = [];
 var resolucao;
@@ -12,6 +12,7 @@ var lion, parot, monkey, uni, personagem;
 var naTela = true;
 var i, j, k;
 var tempoTiro = -1;
+var vida;
 
 function preload(){
     lion = loadImage('leao.png');
@@ -24,6 +25,7 @@ function preload(){
 function setup() {
 	createCanvas(1080, 520);
     frameRate(30);
+    vida=3;
     x = 20;
     y = 360;
     resolucao = 50;
@@ -31,7 +33,7 @@ function setup() {
 		tirox[i] = -50;
 		tirosBS[i] = false;  
 		tiroy [i] = -100; 
-}
+    }
 
     for (i = 0; i <= 3; i++){
         if (i == 0){
@@ -43,11 +45,11 @@ function setup() {
             xe[i] = random(1080,1200);
         }
         if(i == 2){
-            ye[i] = random(400, 450);
+            ye[i] = random(400, 435);
             xe[i] = random(1080,1200);
         }
         if (i == 3){
-            ye[i] = 470;
+            ye[i] = 455;
             xe[i] = random(1080,1200);
         }
     
@@ -56,8 +58,11 @@ function setup() {
 
 function draw() {
     background('#fae');
-    fill(255, 0, 0);
+    fill(0, 0, 255);
     image(personagem, x, y, [70], [70]);
+    textSize(20);
+    text('Vidas: '+vida, 20, 20);
+    text('Munição: ' + municao, 20, 40)
     if (keyIsDown(UP_ARROW)){
         if(y >= 20){
             y = y - 10;
@@ -74,37 +79,86 @@ function draw() {
         for ( i = 0; i < tirosAV; i++) {
             if ( tirosBS[i] ==  false ) {
               tirosBS[i] = true;
+              municao -=1;
               tirox[i] = 100;
               tiroy[i] = y;
               break; 
                 
             } 
-        } 
+        }
     }
     tempoTiro--; 
 
 //colisao do tiro com o inimigo
-/*
-    for(i = 0)
-        if (dist(xe[i]/2, ye[i]/2, x, height/2) < (tamMenor + tamMaior)/2) {
-            fill(255,255,0);
-            ellipse(width/2, height/2, tamMaior, tamMaior);  
-            fill(255,255,255);	  
-        } else  {
-            ellipse(width/2, height/2, tamMaior, tamMaior);  
-        }
-*/
-//
+    //tiro1
+    if (dist(tirox[0], tiroy[0], xe[0], ye[0]) < (25)) {
+        xe[0] = 1080 + random(100, 800);
+        ye[0] = random (30, 480);  
+        tirox[0] = 1180;	  
+    }
+    if (dist(tirox[0], tiroy[0], xe[1], ye[1]) < (25)) {
+        xe[1] = 1080 + random(100, 800);
+        ye[1] = random (30, 480);  
+        tirox[0] = 1180;	  
+    }
+    if (dist(tirox[0], tiroy[0], xe[2], ye[2]) < (25)) {
+        xe[2] = 1080 + random(100, 800);
+        ye[2] = random (30, 480);  
+        tirox[0] = 1180;	  
+    }
+    if (dist(tirox[0], tiroy[0], xe[3], ye[3]) < (25)) {
+        xe[3] = 1080 + random(100, 800);
+        ye[3] = random (30, 480);  
+        tirox[0] = 1180;	  
+    }
+
+    //tiro2
+    if (dist(tirox[1], tiroy[1], xe[0], ye[0]) < (25)) {
+        xe[0] = 1080 + random(100, 800);
+        ye[0] = random (30, 480);  
+        tirox[1] = 1180;	  
+    }
+    if (dist(tirox[1], tiroy[1], xe[1], ye[1]) < (25)) {
+        xe[1] = 1080 + random(100, 800);
+        ye[1] = random (30, 480);  
+        tirox[1] = 1180;	  
+    }
+    if (dist(tirox[1], tiroy[1], xe[2], ye[2]) < (25)) {
+        xe[2] = 1080 + random(100, 800);
+        ye[2] = random (30, 480);  
+        tirox[1] = 1180;	  
+    }
+    if (dist(tirox[1], tiroy[1], xe[3], ye[3]) < (25)) {
+        xe[3] = 1080 + random(100, 800);
+        ye[3] = random (30, 480);  
+        tirox[1] = 1180;	  
+    }
 
 //colisao inimigo com o personagem
-
+    if (dist(x, y, xe[0], ye[0]) < (30)) {
+        vida = vida - 1;
+        xe[0] = 1080 + parseInt(random(0,1000));
+    }
+    if (dist(x, y, xe[1], ye[1]) < (30)) {
+        vida = vida - 1;
+        xe[1] = 1080 + parseInt(random(0,1000));
+    }
+    if (dist(x, y, xe[2], ye[2]) < (30)) {
+        vida = vida - 1;
+        xe[2] = 1080 + parseInt(random(0,1000));
+    }
+    if (dist(x, y, xe[3], ye[3]) < (30)) {
+        vida = vida - 1;
+        xe[3] = 1080 + parseInt(random(0,1000));
+    }
+    //checar tiros
     for ( i = 0; i < tirosAV; i++) {
         if ( tirosBS[i] ) {
-          // tiros 
           ellipse(tirox[i], tiroy[i] + 35, 15, 15 );		    
           tirox[i] += 20; 
           if (tirox[i] > width) {
               tirosBS[i] = false; 
+              municao+=1;
           }
         } 
        
@@ -135,7 +189,7 @@ function draw() {
     for (k = 0; k<=3; k++){
         if (xe[k] < 0){
             xe[k] = 1080 + parseInt(random(0,1000));
-            ye[k] = random(40, 470);
+            ye[k] = random(40, 430);
         }
     }
 } //draw 
